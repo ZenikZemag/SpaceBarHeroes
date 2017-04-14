4e001daa481d24cca37dfa1d9e43aec796d7cc7b
Alejandro Gamez <pg10alejandro@vfs.local>
2017-04-13 19:15:51 -0700

    Adding Project

---
+++ PG10Assignment03_AlejandroGamez/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG10Assignment03_AlejandroGamez {
+    class Game {
+
+
+        public Game() {
+            do {
+                //if(Console.ReadKey(true).Key == ConsoleKey.Backspace) {
+                //    Screen.ClearScreen();
+                //}
+
+                if(Input.KeyDown(ConsoleKey.Spacebar, 0.1f)) {
+                    Input.Print("space");
+                }
+            } while(Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ PG10Assignment03_AlejandroGamez/Screen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PG10Assignment03_AlejandroGamez {
+    class Screen {
+
+
+        //clears the screen and draws the HUD again
+        public static void ClearScreen() {
+            //Clears the screen
+            Console.Clear();
+        }
+    }
+}
